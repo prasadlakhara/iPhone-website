41bde2881b0382415d6ef15a778fdc5a237057b1
prasad <prasadlakhara@gmail.com>
2025-02-14 15:35:35 +0530

    Minor changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,10 +38,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div
-        id="cta"
-        className="flex flex-col items-center opacity-0 translate-y-20"
-      >
+      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
         <a href="#highlights" className="btn">Buy</a>
         <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
